d92f77d8024943e45070411c2cd74d8a4936e3f1
DarkRIDDeR <darkridder.pro@gmail.com>
2019-08-18 19:42:48 +0800

    add fix menu and back to top

--- client/App.vue
+++ client/App.vue
@@ -1,25 +1,32 @@
 <template lang="pug">
     div
-        header
-            .container
-                router-link.logo(to="/"): img(src="@/assets/dlogo.svg", alt="Dlang.ru")
-                nav
-                    router-link(to="/") Главная
-                    router-link(to="/faq") FAQ
-                    router-link(to="/links") Ссылки
-                    router-link(to="/book") Online-учебник
-                    a(href="https://t.me/dlangru" target="_blank")
-                        | Мы в telegram
-                        img(src="@/assets/img/telegram.svg", alt="")
+        fixed-header
+            header
+                .container
+                    router-link.logo(to="/"): img(src="@/assets/dlogo.svg", alt="Dlang.ru")
+                    nav
+                        router-link(to="/") Главная
+                        router-link(to="/faq") FAQ
+                        router-link(to="/links") Ссылки
+                        router-link(to="/book") Online-учебник
+                        a(href="https://t.me/dlangru" target="_blank")
+                            | Мы в telegram
+                            img(src="@/assets/img/telegram.svg", alt="")
+        .header-substrate
         router-view
+        back-to-top(text="", bottom="25px", right="13px")
 </template>
 
 <script>
+import FixedHeader from 'vue-fixed-header';
+import BackToTop from 'vue-backtotop'
 
 export default {
-  name: 'App',
-  methods: {
-  }
+    name: 'App',
+    components: {
+        FixedHeader,
+        BackToTop
+    }
 }
 </script>
 
