528dfe58663711956c8045df8063d6ed88c67d00
DarkRIDDeR <darkridder.pro@gmail.com>
2019-08-18 19:43:03 +0800

    add FAQ

--- client/router/index.js
+++ client/router/index.js
@@ -5,23 +5,28 @@ Vue.use(Router);
 
 import Main from '../views/Main.vue'
 import NotFound from '../views/NotFound.vue'
+import FAQ from '../views/FAQ.vue'
 
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      component: Main
-    },
-    {
-      path: '/404',
-      component: NotFound,
-    },
-    { 
-      path: '*', 
-      redirect: '/404'
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            component: Main
+        },
+        {
+            path: '/faq',
+            component: FAQ
+        },
+        {
+            path: '/404',
+            component: NotFound,
+        },
+        {
+            path: '*',
+            redirect: '/404'
+        }
+    ]
 })
